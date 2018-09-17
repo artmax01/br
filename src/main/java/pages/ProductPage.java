3cc4f67e3b9f8b810fba1bdbb82e9e51ed4e9ff5
Ruslan Levytskyi <ruslanlevytskyi@RLEVYTSKYID.local>
2018-09-17 15:24:14 +0300

    Cross Sell test added

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
@@ -15,10 +16,23 @@ public class ProductPage extends BasePage{
 
     public static SelenideElement
     sizeByDefault = $(By.xpath("//div[@aria-checked='true' and @aria-describedby='option-label-bed_size-177']")),
-    klarnaBlock = $(".klarna-placement");
+    selectedSize = $x("//*[@class='swatch-option text selected']"),
+    selectedSecondOption = $x("//*[@class='swatch-option text active']"),
+    klarnaBlock = $(".klarna-placement"),
+    productPriceText = $x("//*[@class='product-info-price']/div/span/span/span"),
 
+    crossSellModal = $(".xs-modal__sold"),
+    xsCloseButton = $x("/html/body/div[4]/aside[2]/div[2]/header/button"),
+    xsProductNameText = $(".__title m-0"),
+    xsProductSizeText = $x("//*[@class='xs-modal__sold']/div/div/div/div/p[2]"),
+    xsProductPriceText = $x("//*[@class='xs-modal__sold']/div/div/div/p/span/span/span"),
+    xsCheckoutNowButton = $x(".//a[contains(text(), 'Checkout Now ')]"),
+    xsAddBoxSpringsToCartButton = $x("//*[@id='modal-content-38']/div/div/div/div[2]/div[1]/div[2]/button"),
+    xsAddSmartMotionToCartButton = $x("//*[@id='modal-content-38']/div/div/div/div[2]/div[2]/div[2]/button"),
+    xsBoxSpringSizeText = $x("//*[@class='xs-modal']/div[2]/div[1]/p[2]"),
+    xsSmartMotionSizeText = $x("//*[@class='xs-modal']/div[2]/div[2]/p[2]");
 
-    //* Page Methods **/
+    /** Page Methods **/
 
     public static void SelectSize(String size){
         reporter.info("Selecting product size: " + size);
@@ -43,10 +57,18 @@ public class ProductPage extends BasePage{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        if ( $(".xs-modal__sold").isDisplayed()){
+        if (crossSellModal.isDisplayed()){
             reporter.info("closing popup");
-            $x(".//a[contains(text(), 'Checkout Now ')]").click();
+            xsCheckoutNowButton.click();
         }
+        return CartPage.Instance;
+    }
+
+    public static CartPage clickAddToCartDontClosePopup() {
+        reporter.info("Clicking on \"Add to cart\" button");
+        waitForPageToLoad();
+        getElement(By.cssSelector("#product-addtocart-button"))
+                .click();
         return CartPage.Instance;
     }
 
@@ -68,6 +90,11 @@ public class ProductPage extends BasePage{
     public static void verifyKlarnaAbsence(){
         reporter.info("Checking that \"Klarna\" block is not displayed");
         getElement(By.xpath(".//div[@class='klarna-placement']//iframe")).shouldNotBe(Condition.visible);
+    }
+
+    public static void clickXsClose(){
+        reporter.info("Closing Cross Sell Popup");
+        xsCloseButton.click();
     }
 
 }
---
+++ src/test/java/smoke/general_test/Cross_Sell_Test.java
@@ -0,0 +1,58 @@
+package smoke.general_test;
+
+import annotations.TestName;
+import com.codeborne.selenide.ElementsCollection;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.PageFooter;
+import pages.PageHeader;
+import pages.ProductPage;
+import utils.BaseTest;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.open;
+import static pages.BasePage.closeWelcomeMessage;
+import static pages.BasePage.waitForPageToLoad;
+
+public class Cross_Sell_Test extends BaseTest {
+
+    PageHeader header = PageHeader.Instance;
+    ProductPage product = ProductPage.Instance;
+    CartPage cart = CartPage.Instance;
+
+    ElementsCollection
+            sizeButtonsCollection = $$x("//*[@role='option']"),
+            secondOptionButtonsCollection = $$x("//p[@class='swatch-option text'] and //p[@class='swatch-option text active']");
+
+    @Test
+    @TestName (name = "Cross Sell Price and Size Test")
+    public void Cross_Sell_SizePrice_Test(){
+
+        String pdpMattressSize, xsMattressSize, xsBoxSpringsSize, xsSmartMotionSize, pdpMattressPrice;
+
+        open(baseUrl);
+        closeWelcomeMessage();
+        header.openMattressesPlp()
+                .OpenProductPage("CALISTA");
+        for (int x=sizeButtonsCollection.size()-1; x>-1; x--){
+            sizeButtonsCollection.get(x).click();
+            pdpMattressSize = sizeButtonsCollection.get(x).getText();
+            pdpMattressPrice = product.productPriceText.getText();
+            waitForPageToLoad();
+            product.clickAddToCartDontClosePopup();
+            waitForPageToLoad();
+            Assert.assertTrue(product.crossSellModal.isDisplayed(), "Cross Sell modal is not displayed");
+            Assert.assertTrue(pdpMattressSize.equalsIgnoreCase(product.xsProductSizeText.getText()), "Incorrect mattress size. On PDP: "+pdpMattressSize+", on XS: "+product.xsProductSizeText.getText());
+            Assert.assertTrue(pdpMattressPrice.equalsIgnoreCase(product.xsProductPriceText.getText()), "Incorrect mattress price. On PDP: "+pdpMattressPrice+", on XS: "+product.xsProductPriceText.getText());
+            Assert.assertTrue(pdpMattressSize.equalsIgnoreCase(product.xsBoxSpringSizeText.getText()), "Incorrect size of proposed Box Springs. On PDP: "+pdpMattressSize+", on XS: "+product.xsBoxSpringSizeText.getText());
+            if (!pdpMattressSize.equals("FULL")) {
+                Assert.assertTrue(pdpMattressSize.equalsIgnoreCase(product.xsSmartMotionSizeText.getText()), "Incorrect size of proposed Smart Motion. On PDP: "+pdpMattressSize+", on XS: "+product.xsSmartMotionSizeText.getText());
+            }
+            product.clickXsClose();
+        }
+
+    }
+}
